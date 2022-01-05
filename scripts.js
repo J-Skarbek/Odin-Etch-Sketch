faf18d16d9017bce20a3b9f7d53aa3d0c2fda325
J-Skarbek <john@snarkmediaagency.com>
2022-01-05 10:04:05 -0600

    working on adding button functionality to clear the grid

--- scripts.js
+++ scripts.js
@@ -1,27 +1,47 @@
 const wrapper = document.querySelector('.project-wrapper');
+let miniBlock = document.createElement('div');
 
 for (let i = 0; i <= 255; i++){
-  const miniBlock = document.createElement('div');
+  let miniBlock = document.createElement('div');
   wrapper.appendChild(miniBlock);
-  miniBlock.classList.add('mini-block', 'grid-items');
+  miniBlock.classList.add('miniBlock', 'grid-items');
+  miniBlock.addEventListener('mouseover', () => {
+  miniBlock.classList.add('active-etch-block');
+  });
 }
 
-const etchBlocks = document.querySelectorAll('miniBlock');
-
-etchBlocks.forEach((miniBlock) => {
-  etchBlocks.addEventListener('hover', () => {
-    etchBlocks.classList.add('active-etch-block')
-  });
+document.getElementById("clear-grid").addEventListener("click", function() {
+  const clearGrid = document.getElementsByClassName('active-etch-block');
+  for (let i = 0; i <= 255; i++){
+    clearGrid.classList.remove('active-etch-block');
+  }
 });
 
+// function clearGrid() {
+//   miniBlock.classList.remove('active-etch-block');
+// };
+
+// document.getElementById("clear-grid").onclick = clearGrid();
 
 
-// let i = 0;
-// do {
-//   wrapper.appendChild(miniBlock);
-//   miniBlock.classList.add('mini-block');
-//   i++;
-// } while (i <= 10);
+
+// let etchBlocks = document.getElementsByClassName('miniBlock')[1];
+// etchBlocks.addEventListener('mouseover', () => {
+//   miniBlock.classList.add('active-etch-block');
+// });
 
 
- 
+// etchBlocks.forEach((miniBlock) => {
+//   miniBlock.addEventListener('mouseover', () => {
+//     miniBlock.classList.add('active-etch-block');
+//   });
+// });
+
+
+// const etchBlocks = document.querySelectorAll('.miniBlock');
+
+// etchBlocks.addEventListener('mouseover', () => {
+//   miniBlock.classList.add('active-etch-block');
+// });
+
+
