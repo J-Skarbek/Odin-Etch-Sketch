bdc839eaf8ef6a8921fc65a694f89a5a275912dd
J-Skarbek <john@snarkmediaagency.com>
2022-01-22 12:08:06 -0600

    kindof stuck, taking a break for now.

--- scripts.js
+++ scripts.js
@@ -1,9 +1,10 @@
 const wrapper = document.querySelector('.project-wrapper');
+// const clearGrid = Array.from(document.getElementsByClassName('active-etch-block'));
 // let gridSizeTest = document.querySelector('.project-wrapper').style.gridTemplateRows = "repeat(25, 1fr)";
 let miniBlock = document.createElement('div');
 initGrid();
 
-
+//standard B+W Grid
 function initGrid(){
   for (let i = 0; i <= 2499; i++){
     let miniBlock = document.createElement('div');
@@ -12,10 +13,34 @@ function initGrid(){
     miniBlock.addEventListener('mouseover', () => {
     miniBlock.classList.add('active-etch-block');
     });
-  }
+  };
 };
 
+function colorGenerator() {
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+  
+  const rgbBlocks = Array.from(document.getElementsByClassName('miniBlock'));
+  rgbBlocks.forEach(rgbBlocks => {
+    rgbBlocks.querySelector('.miniBlock').style.cssText = "background-color: rgb(" + r + "," + g + "," + b + ");";
+  });
+  // document.querySelector('.miniBlock').style.cssText = "background-color: rgb(" + r + "," + g + "," + b + ");";
+    // const coloredBlocks = document.querySelector('.miniBlock').style.cssText = "background-color: rgb(" + r + "," + g + "," + b + ");";
+  };
 
+
+
+//Listener for colorizing the grid
+document.getElementById("color-button").addEventListener("click", function(){
+  const clearGrid = Array.from(document.getElementsByClassName('active-etch-block'));
+  clearGrid.forEach(clearGrid => {
+    clearGrid.classList.remove('active-etch-block')
+  });
+  colorGenerator();    
+});
+
+//Lisener to clear the grid and resent the number of squares
 document.getElementById("clear-grid").addEventListener("click", function() {
   const clearGrid = Array.from(document.getElementsByClassName('active-etch-block'));
     clearGrid.forEach(clearGrid => {
@@ -27,7 +52,6 @@ document.getElementById("clear-grid").addEventListener("click", function() {
     alert ("Error - too many squares! Pick a value less than 50.");
     sizeDeclaration = '';
     sizeDeclaration = prompt("How big of a grid do you want? (max = 50)");
-    console.log(sizeDeclaration);
     gridSizing();
   } else {
     gridSizing();
@@ -35,7 +59,6 @@ document.getElementById("clear-grid").addEventListener("click", function() {
  
   function gridSizing(){
     let gridDivNumeration = document.querySelector('.project-wrapper').style.cssText = "grid-template-rows:repeat(" + sizeDeclaration + ", 1fr); grid-template-columns:repeat(" + sizeDeclaration + ", 1fr);";
-    console.log(sizeDeclaration);
   };
-  
+
 });
