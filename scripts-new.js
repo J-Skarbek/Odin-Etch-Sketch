d0c12a271d2948c464fce395413785181fa826f2
J-Skarbek <john@snarkmediaagency.com>
2022-06-28 14:02:43 -0500

    Defer the JS and set up event listers to clear the grid.

--- scripts-new.js
+++ scripts-new.js
@@ -1,3 +1,21 @@
+const clearGrid = document.getElementById('btn-1')
+const colorMode = document.getElementById('btn-2')
+const blackWhiteMode = document.getElementById('btn-3')
+
+//Lisener to clear the grid and resent the number of squares
+// document.getElementById("clear-grid").addEventListener("click", function() {
+//   const clearGrid = Array.from(document.getElementsByClassName('active-etch-block'));
+//     clearGrid.forEach(clearGrid => {
+//     clearGrid.classList.remove('active-etch-block');
+//   });
+
+let clearTheGrid = () => {
+  const clearElements = Array.from(document.getElementsByClassName('active-etch-block'))
+  clearElements.forEach(clearElements => {
+    clearElements.classList.remove('active-etch-block')
+  })
+}
+
 let gridInitialize = () => {
   for (let i = 0; i <= 256; i++) {
     let wrapper = document.getElementById('project-wrapper');
@@ -10,4 +28,6 @@ let gridInitialize = () => {
   }
 }
 
+
+clearGrid.addEventListener('click', clearTheGrid)
 window.addEventListener('load', gridInitialize)
